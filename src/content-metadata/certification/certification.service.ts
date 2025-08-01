19e6c8385a0005fe44e7546f4dce33d9bd2cbe49
dibyajc <dibyajyoti.choudhury@vantagecircle.com>
2025-08-02 00:08:02 +0530

    Added certification controllers and services

--- src/content-metadata/certification/certification.service.ts
+++ src/content-metadata/certification/certification.service.ts
@@ -1,26 +1,111 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCertificationDto } from './dto/create-certification.dto';
 import { UpdateCertificationDto } from './dto/update-certification.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Certification } from './entities/certification.entity';
+import { Repository } from 'typeorm';
+import { Region } from '../region/entities/region.entity';
 
 @Injectable()
 export class CertificationService {
-  create(createCertificationDto: CreateCertificationDto) {
-    return 'This action adds a new certification';
+  constructor(
+    @InjectRepository(Certification)
+    private readonly certificationRepository: Repository<Certification>,
+    @InjectRepository(Region)
+    private readonly regionRepository: Repository<Region>,
+  ) {}
+
+  async createCertification(
+    createCertificationDto: CreateCertificationDto,
+  ): Promise<Certification> {
+    const existingCert = await this.certificationRepository.findOne({
+      where: { name: createCertificationDto.code },
+    });
+
+    if (existingCert) {
+      throw new ConflictException('Certification already exists');
+    }
+
+    const newCert = this.certificationRepository.create({
+      ...createCertificationDto,
+      region: { id: createCertificationDto.regionId },
+    });
+
+    return await this.certificationRepository.save(newCert);
   }
 
-  findAll() {
-    return `This action returns all certification`;
+  findAllCertifications(): Promise<Certification[]> {
+    return this.certificationRepository.find({ order: { code: 'asc' } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} certification`;
+  async findCertificationById(id: number): Promise<Certification> {
+    const certification = await this.certificationRepository.findOne({
+      where: { id },
+    });
+
+    if (!certification) {
+      throw new NotFoundException(`Certification with ID ${id} not found`);
+    }
+
+    return certification;
   }
 
-  update(id: number, updateCertificationDto: UpdateCertificationDto) {
-    return `This action updates a #${id} certification`;
+  async updateCertification(
+    id: number,
+    updateCertificationDto: UpdateCertificationDto,
+  ) {
+    const certification = await this.certificationRepository.findOne({
+      where: { id },
+    });
+
+    if (!certification) {
+      throw new NotFoundException(`Certification with ID ${id} not found`);
+    }
+
+    let region: Region | null = null;
+
+    if (updateCertificationDto.regionId) {
+      region = await this.regionRepository.findOne({
+        where: { id: updateCertificationDto.regionId },
+      });
+
+      if (!region) {
+        throw new BadRequestException(
+          `Region with ID ${updateCertificationDto.regionId} does not exist`,
+        );
+      }
+    }
+
+    delete updateCertificationDto.regionId;
+
+    const updatePayload: Partial<Certification> = {
+      ...updateCertificationDto,
+    };
+
+    if (region) {
+      updatePayload.region = region;
+    }
+
+    const updated = this.certificationRepository.merge(
+      certification,
+      updatePayload,
+    );
+
+    await this.certificationRepository.save(updated);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} certification`;
+  async deleteCertification(id: number) {
+    const certification = await this.findCertificationById(id);
+
+    if (!certification) {
+      throw new NotFoundException(`Certification with ID ${id} not found`);
+    }
+
+    await this.certificationRepository.remove(certification);
   }
 }
